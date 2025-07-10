d876bcd18d2546ebb059741a6b3a2d4f9d204a9f
Abigaëlle Panhelleux <100417910+LunaPak14@users.noreply.github.com>
2025-07-10 16:51:53 +0200

    [fix] fixed the link to the logo to the home page

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { TitleCasePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [ TitleCasePipe, MatToolbarModule ],
+  imports: [ TitleCasePipe, MatToolbarModule, RouterLink ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
